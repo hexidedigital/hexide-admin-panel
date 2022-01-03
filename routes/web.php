ccdf2c4371127eefe20bf888c4e7e1de2d5de0dd
moik.olesandr <moik.o@hexide-digital.com>
2022-01-03 17:00:18 +0200

    code style update

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use HexideDigital\HexideAdmin\Http\Controllers\Frontend\LanguageController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('set-locale/{locale?}', LanguageController::class)->name('front.set-locale');
+Route::get('set-locale/{locale?}', LanguageController::class)->name('front.setLocale');
 
 Route::post('generate/slug', function (\Illuminate\Http\Request $request){
     return str_slug($request->get('value', ''));
