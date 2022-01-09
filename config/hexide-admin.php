a9fa9360e3a0e2f7ca110241d0c55758146593ed
moik.olesandr <moik.o@hexide-digital.com>
2022-01-09 22:35:58 +0200

    update config file

--- config/hexide-admin.php
+++ config/hexide-admin.php
@@ -2,7 +2,10 @@
 
 return [
 
-    'secret_key' => null,
+    'secret_key' => null, //env('ADMIN_SECRET', null)
+
+    // -------------------------------
+    // Localisation
 
     'locale' => 'uk',
     'lang_cookie' => 'admin_locale',
@@ -10,7 +13,7 @@ return [
     // locales only for admin panel
     'locales' => [
         'uk',
-        'ru',
+        // 'ru',
         'en',
     ],
 
@@ -20,12 +23,15 @@ return [
 //        'uk' => 'ua',
     ],
 
+    // -------------------------------
+    // Generate command
+
     // in this place you can edit recommended namespaces for files
     'namespaces' => [
-        'model' => 'App\\Models',
-        'request' => 'App\\Http\\Requests\\Backend',
-        'service' => 'App\\Services\\Backend',
-        'controller' => 'App\\Http\\Controllers\\Backend',
+        'model' => 'App\\Models\\',
+        'request' => 'App\\Http\\Requests\\Backend\\',
+        'service' => 'App\\Services\\Backend\\',
+        'controller' => 'App\\Http\\Controllers\\Backend\\',
         'livewire-table' => 'App\\Http\\Livewire\\Admin\\Tables',
     ],
 
@@ -44,6 +50,33 @@ return [
         // no need to set stub-key
         // will simply be placed in this place module blade views
         'views' => 'resources/views/admin/view/',
+    ],
+
+    // -------------------------------
+    // Some design properties
+
+    'with_preloader' => true,
+    'preloader' => [
+        'image' => 'img/HexideDigitalLogo.png',
+        'height' => 90,
+        'width' => 90,
+        'animate_name' => 'bounceOut animate__infinite',
+    ],
+
+    // -------------------------------
+    // Variables, admin configurations
+
+    'variables' => [
+        'key_1' => [
+            // 'key' => 'key_1',
+            'name' => 'Name',
+            'type' => \HexideDigital\HexideAdmin\Models\AdminConfiguration::TEXT,
+            // 'translatable' => true
+            'localization' => [
+                'uk' => 'Some value or json string',
+            ],
+            // 'plain_value' => 'Plain text or html',
+        ],
     ],
 
 ];
