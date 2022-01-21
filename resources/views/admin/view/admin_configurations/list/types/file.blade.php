078acfc876b8b8e649ed6da004fd93fb4aac1cb8
moik.olesandr <moik.o@hexide-digital.com>
2022-01-21 03:44:21 +0200

    style fixes

--- resources/views/admin/view/admin_configurations/list/types/file.blade.php
+++ resources/views/admin/view/admin_configurations/list/types/file.blade.php
@@ -3,16 +3,19 @@
 <div x-data="imagePreview('{{fu_url($inputValue ?? '')}}')">
     <div class="custom-file">
         <input type="file" class="custom-file-input" accept="*/*"
-               name="{{$nameInputCase.'[file]'}}" id="{{$nameDotCase}}"
+               name="{{"{$nameInputCase}[file]"}}" id="{{$nameDotCase}}"
                @change="setPreview($event)">
         <label class="custom-file-label" for="{{$nameDotCase}}" x-text="fileName"></label>
     </div>
+
+    <a x-show="image" @if(empty($inputValue)) style="display: none" @endif
+       href="{{fu_url($inputValue ?? '')}}" target="_blank">{{fu_url($inputValue ?? '')}}</a>
 
     <p class="pt-3">
         <button type="button" class="btn btn-warning btn-sm" x-show="image" @click.prevent="removeImage"
                 @if(empty($inputValue)) style="display: none" @endif>
             {!! __('hexide-admin::buttons.delete') !!}
         </button>
-        <input type="hidden" name="{{$nameInputCase.'[isRemoveFile]'}}" :value="is_image_deleted">
+        <input type="hidden" name="{{"{$nameInputCase}[isRemoveFile]"}}" :value="is_image_deleted">
     </p>
 </div>
--- resources/views/admin/view/admin_configurations/list/types/image.blade.php
+++ resources/views/admin/view/admin_configurations/list/types/image.blade.php
@@ -1,7 +1,7 @@
 <div x-data="imagePreview('{{fu_url($inputValue ?? '')}}')">
     <div class="custom-file">
         <input type="file" class="custom-file-input" accept="image/*"
-               name="{{$nameInputCase.'[image]'}}" id="{{$nameDotCase}}"
+               name="{{"{$nameInputCase}[image]"}}" id="{{$nameDotCase}}"
                @change="setPreview($event)">
         <label class="custom-file-label" for="{{$nameDotCase}}" x-text="fileName"></label>
     </div>
@@ -13,6 +13,6 @@
                 @if(empty($inputValue)) style="display: none" @endif>
             {!! __('hexide-admin::buttons.delete') !!}
         </button>
-        <input type="hidden" name="{{$nameInputCase.'[isRemoveImage]'}}" :value="is_image_deleted">
+        <input type="hidden" name="{{"{$nameInputCase}[isRemoveImage]"}}" :value="is_image_deleted">
     </p>
 </div>
