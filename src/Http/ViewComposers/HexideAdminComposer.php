d70b35b3cb387c70c2fd0266d44257b14f4aaa78
Oleksand-Moik <sashamoik2016@gmail.com>
2022-01-25 19:45:43 +0200

    fixes

--- src/Http/ViewComposers/HexideAdminComposer.php
+++ src/Http/ViewComposers/HexideAdminComposer.php
@@ -34,7 +34,7 @@ class HexideAdminComposer
     public function getToggleAttributes(): Collection
     {
         return collect([
-            'status' => collect([
+            'status' => [
                 'data-on' => '<i class="fas fa-eye"></i>',
                 'data-off' => '<i class="fas fa-eye-slash"></i>',
                 'data-onstyle' => 'success',
@@ -42,14 +42,14 @@ class HexideAdminComposer
                 'data-width' => '75',
                 'data-size' => 'small',
                 'class' => 'toggle_attributes',
-            ]),
-            'state_read' => collect([
+            ],
+            'state_read' => [
                 'data-on' => '<i class="far fa-envelope-open"></i>',
                 'data-off' => '<i class="far fa-envelope"></i>',
                 'data-onstyle' => 'default',
                 'data-offstyle' => 'primary',
                 'class' => 'toggle_attributes',
-            ]),
+            ],
         ]);
     }
 }
