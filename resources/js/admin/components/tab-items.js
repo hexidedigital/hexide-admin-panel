1d1d314b1f4e580d77e1d87deefaa8659193a200
Oleksand-Moik <sashamoik2016@gmail.com>
2022-04-20 17:18:54 +0300

    feat: updated tab-items.js for working with more than one tab group on page

--- resources/js/admin/components/tab-items.js
+++ resources/js/admin/components/tab-items.js
@@ -1,30 +1,35 @@
-export default (items = []) => ({
+export default (items = [], suffix = '', push_state = true) => ({
     active_tab: null,
+    key_name: null,
     items: items,
     original_title: null,
 
     init() {
+        this.key_name = ['active_tab', suffix].filter(v => v).join('_');
         this.original_title = document.title
-        this.active_tab = new URLSearchParams(window.location.search).get('active_tab') || this.items[0]?.name;
+        this.active_tab = new URLSearchParams(window.location.search).get(this.key_name) || this.items[0]?.name;
     },
 
     isActive(item) {
         if (this.active_tab) {
             return item === this.active_tab
         } else {
-            return this.getItem(item) === this.items[0] && this.setActive(this.items[0].name)
+            return this.getItem(item) === this.items[0] && this.setActive(this.items[0]?.name)
         }
     },
 
     setActive(item) {
-        if (!this.getItem(item)) item = this.items[0].name
+        if (!this.getItem(item)) item = this.items[0]?.name
 
-        const pathname = window.location.pathname + "?active_tab=" + item;
-        const el = this.getItem(item);
+        if (!suffix || push_state) {
+            const searchParams = new URLSearchParams(window.location.search);
+            searchParams.set(this.key_name, item)
 
-        window.history.pushState(null, null, pathname);
-        // const title = el.title ? el.title + ' | ' : '';
-        // document.title = title + this.original_title;
+            const pathname = window.location.pathname + '?' + searchParams.toString()
+
+            window.history.replaceState(null, null, pathname);
+        }
+
         this.active_tab = item
 
         return true;
