bf4d64ff2950eb3731cd122cab867d8ec839b640
Oleksandr-Moik <sashamoik2016@gmail.com>
2022-05-15 15:46:38 +0300

    fix

--- resources/views/admin/partials/ajax/input.blade.php
+++ resources/views/admin/partials/ajax/input.blade.php
@@ -12,18 +12,15 @@
 @endphp
 
 <label>
-    <input type="checkbox" class="toogle ajax_ckeckbox {{$module.$model->getKey().$field}}"
+    <input
+        type="{!! $type !!}"
+        class="ajax_input form-control {{$module.$model->getKey().$field}}"
+        style="max-width: 100px"
+        value="{!! $model->getAttribute($field) !!}"
         data-id="{!! $model->getKey() !!}"
         data-token="{!! csrf_token() !!}"
         data-field="{!! $field !!}"
         data-url="{!! $route !!}"
-        data-value="{!! !$model->getAttribute($field) !!}"
-
-        @foreach($data as $item => $val) data-{{$item}}="{{$val}}" @endforeach
-        @foreach($attributes as $item => $val) {{$item}}="{{$val}}" @endforeach
-        data-width="75"
-        data-size="small"
-
-        {!! ($model->getAttribute($field) ? 'checked="checked"' : '') !!}
+        data-value="{!! $model->getAttribute($field) !!}"
     />
 </label>
--- resources/views/admin/partials/ajax/toggler.blade.php
+++ resources/views/admin/partials/ajax/toggler.blade.php
@@ -27,18 +27,24 @@
 @endphp
 
 <label>
-    <input type="checkbox" class="toogle ajax_ckeckbox {{$module.$model->getKey().$field}}"
+    <input
+        type="checkbox"
+        class="toogle ajax_ckeckbox {{$module.$model->getKey().$field}}"
         data-id="{!! $model->getKey() !!}"
         data-token="{!! csrf_token() !!}"
         data-field="{!! $field !!}"
         data-url="{!! $route !!}"
         data-value="{!! !$model->getAttribute($field) !!}"
-
-        @foreach($data as $item => $val) data-{{$item}}="{{$val}}" @endforeach
-        @foreach($attributes as $item => $val) {{$item}}="{{$val}}" @endforeach
         data-width="75"
         data-size="small"
 
-        {!! ($model->getAttribute($field) ? 'checked="checked"' : '') !!}
+    @foreach($data as $item => $val)
+        {{'data-'.$item}}="{{$val}}"
+    @endforeach
+    @foreach($attributes as $item => $val)
+        {{$item}}="{{$val}}"
+    @endforeach
+
+    {!! ($model->getAttribute($field) ? 'checked="checked"' : '') !!}
     />
 </label>
