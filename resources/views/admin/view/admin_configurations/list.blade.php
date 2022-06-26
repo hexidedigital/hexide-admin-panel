d84e8971408f040ab741b28713d78fefbab4d85b
Oleksandr-Moik <sashamoik2016@gmail.com>
2022-06-26 18:14:12 +0300

    admin-configurations - small fix for translating tabs

--- resources/views/admin/view/admin_configurations/list.blade.php
+++ resources/views/admin/view/admin_configurations/list.blade.php
@@ -13,21 +13,24 @@
     <div class="row" x-data="TabItems">
         <div class="col-3 col-md-2">
             <div class="nav flex-column nav-tabs h-100 border-right-0">
-                @foreach($admin_configuration_groups ?? [] as $name => $admin_configurations)
+                @foreach($admin_configuration_groups ?? [] as $groupName => $admin_configurations)
                     @php
-                        $tab = __("hexide-admin::admin_labels.admin_configuration_tab.$name");
-                        if($tab == "hexide-admin::admin_labels.admin_configuration_tab.$name") $tab = $name;
+                        if(trans()->has($tabText = "admin_labels.admin_configuration_tab.$groupName")) {
+                            $tabText = trans($tabText);
+                        } else {
+                            $tabText = $groupName;
+                        }
                     @endphp
 
-                    <x-hexide-admin::tabs.tab-link :name="$name" :text="$tab"/>
+                    <x-hexide-admin::tabs.tab-link :name="$groupName" :text="$tabText"/>
                 @endforeach
             </div>
         </div>
 
         <div class="col-9 col-sm-10">
             <div class="tab-content">
-                @foreach($admin_configuration_groups ?? [] as $name => $admin_configurations)
-                    <x-hexide-admin::tabs.tab-content :name="$name">
+                @foreach($admin_configuration_groups ?? [] as $groupName => $admin_configurations)
+                    <x-hexide-admin::tabs.tab-content :name="$groupName">
                         @foreach($admin_configurations as $admin_configuration)
                             @include("hexide-admin::admin.view.admin_configurations.list.card_form")
                         @endforeach
